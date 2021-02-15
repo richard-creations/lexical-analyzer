da299d8bb0caedc20cda0409f42b4eacd2328187
Richard Maduka <34720125+richard-creations@users.noreply.github.com>
2021-02-14 21:55:46 -0600

    Update differ.py

--- differ.py
+++ differ.py
@@ -3,12 +3,12 @@ from os.path import isfile, join, splitext
 from glob import glob
 import subprocess
 
-def go():
+def differ():
     onlyfiles = [y for x in walk("./samples")
-                 for y in glob(path.join(x[0], '*.out'))]
+                 for y in glob(path.join(x[0], '*.frag'))]
     for file in onlyfiles:
         filename, file_extension = path.splitext(file)
         name = filename.split("/")
         output = name[len(name) - 1]
-        cmd = "dcc < ./samples/"+output+".out > ./result/"+output+".out"
+        cmd = "diff ./samples/"+output+".frag  ./result/"+output+".out > ./diffs/"+output+".diff"
         subprocess.Popen(cmd, shell=True)
